c5c4956cc5587f5952941095f3326dedd88c3213
Mohammed Musharraf <musharraf.personal@gmail.com>
2023-09-21 17:50:19 +0530

    lec 3

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// };
+
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://www.linkedin.com', target:'_blank'},
+  'visit linkedin',
+ 
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    // <App />
+    reactElement
+ 
 )
